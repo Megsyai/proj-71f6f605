38e78684209d1d214553226675fc39f2a5220bd0
abdallamohamed55v-cmyk <abdallamohamed55v@gmail.com>
2026-05-22 21:16:58 +0300

    Update src/pages/Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,17 @@
+import { HeroSection } from "@/components/sections/HeroSection";
+import { LegacySection } from "@/components/sections/LegacySection";
+import { TimelineSection } from "@/components/sections/TimelineSection";
+import { MusicPreviewSection } from "@/components/sections/MusicPreviewSection";
+
+const Index = () => {
+  return (
+    <main>
+      <HeroSection />
+      <LegacySection />
+      <TimelineSection />
+      <MusicPreviewSection />
+    </main>
+  );
+};
+
+export default Index;
